400c506561f05ccb4860e232ad74be685013657d
galiazzi-17 <murilo.galiazzi@gmail.com>
2023-08-10 10:54:50 -0300

    javascript finalizado

--- js/javascript.js
+++ js/javascript.js
@@ -3,4 +3,11 @@ var tooggleMode = () => {
 
  html.classList.toggle("light");
 
+ const isLight = html.classList.contains("light");
+ const srcValue = isLight ? "../img/Among-Us-feat.jpg" : "../img/avatar.jpg";
+ const altValue = isLight ? "Imagem de perfil com um avatar na cor vermelha" : "Imagem de perfil com um avatar na cor marrom";
+
+
+ document.querySelector("#profile img").setAttribute("src",srcValue);
+ document.querySelector("#profile img").setAttribute("alt",altValue);
 };
